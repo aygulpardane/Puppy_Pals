e7f87f6a89bee017bdca02fa1a115c1769904b47
aygulpardane <aygulparpucu@gmail.com>
2023-09-11 11:40:23 -0600

    add key

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import {puppyList} from './data.js'
+import {puppyList} from './data.js' // we pass the puppyList array from the data.js file as the default value
 import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  // first element is the value that you are storing, the second is a function, which you can use to reset the value:
+  // use state returns an array
   const [puppies, setPuppies] = useState(puppyList);
 
   console.log(puppies);
@@ -12,11 +14,11 @@ function App() {
     <div>
       {
         puppies.map((puppy) => {
-          return <p>{puppy.name}</p>
+          return <p key ={puppy.id}>{puppy.name}</p>
         })
-      };
+      }
     </div>
-  )
+  );
 }
 
 export default App
